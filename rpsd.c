835a00fe44d78275896a9b45e7a80e32d3ba1824
Aditya Shah <reachadishah@gmail.com>
2025-04-27 20:49:48 -0400

    latest changes

--- rpsd.c
+++ rpsd.c
@@ -14,6 +14,14 @@
 #define BACKLOG 10
 #define BUFFER_SIZE 1024
 
+typedef struct {
+    int fd;
+    char name[200];
+} player;
+
+player players_queue[1024];
+int count = 0;
+
 void error_exit(const char *msg) {
     perror(msg);
     exit(EXIT_FAILURE);
@@ -44,6 +52,37 @@ int setup_server_socket(int port) {
     return sockfd;
 }
 
+char* find_winner(char *move1, char *move2) {
+    if (strcmp(move1, move2) == 0) {
+        return "D";
+    }
+
+    if ((strcmp(move1, "ROCK") == 0 && strcmp(move2, "SCISSORS") == 0) || (strcmp(move1, "SCISSORS") == 0 && strcmp(move2, "PAPER") == 0) || (strcmp(move1, "PAPER") == 0 && strcmp(move2, "ROCK") == 0)) {
+        return "W";
+    } else {
+        return "L";
+    }
+}
+
+void parse_player(char *message, char *name) {
+    char *start = strchr(message, '|');
+    start += 1;
+    char *end = strchr(start, '|');
+    size_t len = end - start;
+    strncpy(name, start, len);
+    name[len] = '\0';
+}
+
+void send_message(int sock, char *type, char *arg1, char *arg2) {
+    char message[1024];
+    snprintf(message, sizeof(message), "%s|%s|%s||", type, arg1, arg2);
+    send(sock, message, strlen(message), 0);
+}
+
+void handle_game(int fd1, char *name1, int fd2, char *name2) {
+    
+}
+
 void handle_client(int client_fd) {
     char buffer[BUFFER_SIZE];
     ssize_t bytes;
@@ -53,6 +92,54 @@ void handle_client(int client_fd) {
 
     //REMOVE THIS PRINT
     printf("Handled client (fd: %d)\n", client_fd);
+    bytes = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+    buffer[bytes] = '\0';
+    
+    if (buffer[0] != 'P') {
+        close(client_fd);
+        exit(1);
+    }
+
+    char pname[200];
+    parse_player(buffer, pname);
+
+    if (count >= 1024) {
+        close(client_fd);
+        exit(1);
+    }
+
+    players_queue[count].fd = client_fd;
+    strncpy(players_queue[count].name, pname, 200);
+    count += 1;
+
+    if (count >= 2) {
+        int fd1 = players_queue[0].fd;
+        int fd2 = players_queue[1].fd;
+        char name1[200], name2[200];
+        strncpy(name1, players_queue[0].name, 200);
+        strncpy(name2, players_queue[1].name, 200);
+
+        for (int i = 2; i < count; i++) {
+            players_queue[i-2] = players_queue[i];
+        }
+        count -= 2;
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            //handle_game()
+        } 
+        else if (pid > 0) {
+            close(fd1);
+            close(fd2);
+            exit(0);
+        }
+        else {
+            perror("fork");
+            close(fd1);
+            close(fd2);
+            exit(1);
+        }
+    }
 
     //Read P|name|| from client and store it
     //then: Add this client to a shared queue for pairing
